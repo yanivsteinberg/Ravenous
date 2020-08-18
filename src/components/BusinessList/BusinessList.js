e727f2e25508c5929499afbaf8d31e83a5745e04
Yaniv Steinberg <yaniv.steinberg@mail.huji.ac.il>
2020-08-18 15:15:16 +0300

    revenous second version

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -8,19 +8,15 @@ class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
+        {
+          this.props.businesses.map((business) => {
+            return <Business business={business} />;
+          })
+        }
       </div>
-    )
+    );
   }
-};
+}
 
 
 export default BusinessList;
